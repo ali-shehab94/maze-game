b4f36be1cea5379d769c0c88c81a1adc01808ef5
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-24 01:15:05 +0300

    started a new JS file from scratch because I gave up on the old code
    however I now have madea cleaner, better functioning code

--- game.js
+++ game.js
@@ -1,60 +1,63 @@
-
-
 window.onload = function() {
     let score = 0;
-    let lost = false;
-    let game = document.getElementById("start")
-    let startButton = document.getElementById("start");
-    let walls = document.getElementsByClassName("boundary");
-    let scoreboard = document.getElementsByClassName("example");
-    let end = document.getElementById("end");
-    startButton.addEventListener("mouseover", gameStart);
+    let pause = false;
+    const status = document.getElementById("status")
+    const startButton = document.getElementById("start");
+    const walls = document.getElementsByClassName("boundary");
+    const scoreboard = document.getElementsByClassName("example");
+    const end = document.getElementById("end");
 
-    function displayScore(score){
+    startButton.onmouseenter = function() {
+        scoreboard[0].innerHTML = score;
+        startGame();
+        pause = false;
+    }
+
+    startButton.onclick = function() {
+        resetScore()
+    }
+
+    function userWins() {
+        pause = true;
+        status.innerHTML = "You win";
+        score += 5;
         scoreboard[0].innerHTML = score;
     }
 
-    function resetStatus() {
-        let status = document.getElementById("status")
-            status.innerHTML = "Begin by moving your mouse over the 'S'."
-    }
-
-    function win() {
-        score += 5;
-        let status = document.getElementById("status")
-        status.innerHTML = "You win";
-        displayScore(score)
-        lost = true;
-        return
-    }
-
-    function gameOver() {
-        let status = document.getElementById("status");
-        score -= 10;
-        displayScore(score)
-        lost = true;
-        status.innerHTML = "You lost";
-        for (let item of walls) {
-        item.style.backgroundColor = 'red';       
-        }
-    }
-
-    function gameStart() {
-        resetStatus();
-        displayScore(score);
-        //reset background color for walls
+    function resetScore() {
+        score = 0;
+        scoreboard[0].innerHTML = score;
         for (let item of walls) {
             item.style.backgroundColor = null; 
         }
-        // iterate through array that came from walls class
-        for (var i = 0; i < walls.length - 1; i++) {
-            let wall = walls[i]
-            // when wall is touched
-            wall.addEventListener("mouseover", function() {
-                gameOver()
-            }, {once : true});
+    }
+
+    function hitWall() {
+        if (!pause){
+            console.log("lost turned to true")
+            status.innerHTML = "You lose";
+            score -= 10;
+            pause = true;
+            for (let item of walls) {
+            item.style.backgroundColor = "red"; 
+            }
+        }scoreboard[0].innerHTML = score;
+    }
+
+    function startGame() {
+        scoreboard[0].innerHTML = score;
+        console.log("started")
+        for (let item of walls) {
+            item.style.backgroundColor = null; 
+            item.onmouseenter = function() {
+                hitWall()
+            }
         }
-        end.addEventListener("mouseover", win);
-        return
-    }    
+        end.onmouseenter = function() {
+            if (!pause) {
+                console.log("+5")
+                userWins()
+            }
+        }
+    }
 }
