c36b35f12efe1fcae293f5913333e428fec8c119
ali-shehab94 <ali.shehab.ut@gmail.com>
2022-05-24 03:17:40 +0300

    added timer feature, live timer only

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 window.onload = function() {
     let score = 0;
     let pause = false;
+    let int = null;
     const status = document.getElementById("status")
     const startButton = document.getElementById("start");
     const walls = document.getElementsByClassName("boundary");
     const scoreboard = document.getElementsByClassName("example");
     const end = document.getElementById("end");
+    const timerDisplay = document.getElementsByTagName("p")[0]
+
 
     startButton.onmouseenter = function() {
         scoreboard[0].innerHTML = score;
         startGame();
+        resetTimer();
+        startTimer();
         pause = false;
     }
 
@@ -17,11 +22,13 @@ window.onload = function() {
         resetScore()
     }
 
+
     function userWins() {
         pause = true;
         status.innerHTML = "You win";
         score += 5;
         scoreboard[0].innerHTML = score;
+        pauseTimer()
     }
 
     function resetScore() {
@@ -32,9 +39,8 @@ window.onload = function() {
         }
     }
 
-    function hitWall() {
+    function gameOver() {
         if (!pause){
-            console.log("lost turned to true")
             status.innerHTML = "You lose";
             score -= 10;
             pause = true;
@@ -42,22 +48,52 @@ window.onload = function() {
             item.style.backgroundColor = "red"; 
             }
         }scoreboard[0].innerHTML = score;
+        pauseTimer();
+
     }
 
     function startGame() {
         scoreboard[0].innerHTML = score;
-        console.log("started")
         for (let item of walls) {
             item.style.backgroundColor = null; 
             item.onmouseenter = function() {
-                hitWall()
+                gameOver()
             }
         }
         end.onmouseenter = function() {
             if (!pause) {
-                console.log("+5")
                 userWins()
             }
         }
     }
+
+
+    function startTimer() {
+        if(int!==null){
+            clearInterval(int);
+        }
+        int = setInterval(displayTimer,10);
+    } 
+
+    function pauseTimer() {
+        clearInterval(int);
+    }
+
+    function resetTimer() {
+        clearInterval(int);
+        [milliseconds,seconds] = [0,0];
+        timerDisplay.innerHTML = '00 : 00';
+    }
+
+    function displayTimer(){
+        milliseconds+=10;
+        if(milliseconds == 1000){
+            milliseconds = 0;
+            seconds++;
+        }
+        let s = seconds < 10 ? "0" + seconds : seconds;
+        let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+        timerDisplay.innerHTML = `Live: ${s} : ${ms}`;
+    }
+
 }
